864ed75b6202bced928f95de82f951671c1f07fc
Vi <kamurojunrenka@gmail.com>
2025-05-12 04:44:37 -0500

    implement and link ac_npc_mask_cat.c

--- include/ac_npc_mask_cat.h
+++ include/ac_npc_mask_cat.h
@@ -3,10 +3,17 @@
 
 #include "types.h"
 #include "m_actor.h"
-
+#include "ac_npc.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef struct npc_mask_cat_s NPC_MASK_CAT_ACTOR;
+
+/* sizeof(npc_mask_cat_s) == 0x994*/
+struct npc_mask_cat_s {
+    /* 0x000 */ NPC_ACTOR npc_class;
+};
 
 extern ACTOR_PROFILE Npc_Mask_Cat_Profile;
 
