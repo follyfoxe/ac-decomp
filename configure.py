ce0aa347eef11b8bb8bb30599fa1318e4873e156
Cuyler36 <Cuyler36@users.noreply.github.com>
2025-05-25 07:07:31 -0400

    Implement & link ac_npc_post_man

--- configure.py
+++ configure.py
@@ -1131,7 +1131,7 @@ config.libs = [
             Object(Matching, "actor/npc/ac_npc_police.c"),
             Object(NonMatching, "actor/npc/ac_npc_police2.c"),
             Object(Matching, "actor/npc/ac_npc_post_girl.c"),
-            Object(NonMatching, "actor/npc/ac_npc_post_man.c"),
+            Object(Matching, "actor/npc/ac_npc_post_man.c"),
             Object(Matching, "actor/npc/ac_npc_rcn_guide.c"),
             Object(Matching, "actor/npc/ac_npc_rcn_guide2.c"),
             Object(NonMatching, "actor/npc/ac_npc_restart.c"),
--- include/ac_npc_post_man.h
+++ include/ac_npc_post_man.h
@@ -2,11 +2,36 @@
 #define AC_NPC_POST_MAN_H
 
 #include "types.h"
-#include "m_actor.h"
+#include "ac_npc.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+enum {
+    aPMAN_BIRTH_PLAYER_HOUSE,
+    aPMAN_BIRTH_POST_OFFICE,
+
+    aPMAN_BIRTH_NUM
+};
+
+typedef struct npc_post_man_actor_s NPC_POST_MAN;
+typedef void (*aPMAN_PROC)(NPC_POST_MAN* actor, GAME_PLAY* play);
+
+struct npc_post_man_actor_s {
+    NPC_ACTOR npc_class;
+    aPMAN_PROC act_proc;
+    u8 action;
+    u8 next_action;
+    u8 post_office_direct;
+    s8 delivery_idx;
+    s8 move_idx;
+    s8 now_idx;
+    u8 cull_flag;
+    u8 talk_permit;
+    u8 talk_type;
+    f32 ground_y;
+};
 
 extern ACTOR_PROFILE Npc_Post_Man_Profile;
 
@@ -15,4 +40,3 @@ extern ACTOR_PROFILE Npc_Post_Man_Profile;
 #endif
 
 #endif
-
