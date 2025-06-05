cb02183205b4c5fbcc31bff53598c02d6c2c5add
Cuyler36 <Cuyler36@users.noreply.github.com>
2025-06-05 14:24:50 -0400

    Link ef_turi_mizu, cleanup ef_tape

--- src/effect/ef_turi_mizu.c
+++ src/effect/ef_turi_mizu.c
@@ -9,7 +9,6 @@ static void eTM_init(xyz_t pos, int prio, s16 angle, GAME* game, u16 item_name, 
 static void eTM_ct(eEC_Effect_c* effect, GAME* game, void* ct_arg);
 static void eTM_mv(eEC_Effect_c* effect, GAME* game);
 static void eTM_dw(eEC_Effect_c* effect, GAME* game);
-static void eTM_CallEffect(xyz_t pos, int prio, s16 angle, GAME* game, u16 item_name, s16 arg0, s16 arg1);
 
 eEC_PROFILE_c iam_ef_turi_mizu = {
     // clang-format off
@@ -23,6 +22,56 @@ eEC_PROFILE_c iam_ef_turi_mizu = {
     // clang-format on
 };
 
+static void eTM_CallEffect(xyz_t pos, int prio, s16 angle, GAME* game, u16 item_name, s16 arg0, s16 arg1) {
+    switch (arg0) {
+        case 0: {
+            xyz_t hane_pos;
+
+            eEC_CLIP->effect_make_proc(eEC_EFFECT_TURI_HAMON, pos, 1, angle, game, EMPTY_NO, 0, 0);
+
+            hane_pos = pos;
+            hane_pos.y += 3.0f;
+            hane_pos.z += 5.0f;
+            eEC_CLIP->effect_make_proc(eEC_EFFECT_TURI_HANE0, hane_pos, 1, angle, game, EMPTY_NO, 0, 0);
+            break;
+        }
+
+        case 1: {
+            eEC_CLIP->effect_make_proc(eEC_EFFECT_TURI_HAMON, pos, 1, angle, game, EMPTY_NO, 1, 0);
+            break;
+        }
+
+        case 2: {
+            eEC_CLIP->effect_make_proc(eEC_EFFECT_TURI_HAMON, pos, 1, angle, game, EMPTY_NO, 0, 0);
+            break;
+        }
+
+        case 3: {
+            xyz_t hane_pos;
+
+            eEC_CLIP->effect_make_proc(eEC_EFFECT_TURI_HAMON, pos, 1, angle, game, EMPTY_NO, 0, 0);
+
+            hane_pos = pos;
+            hane_pos.y += 3.0f;
+            hane_pos.z += 5.0f;
+            eEC_CLIP->effect_make_proc(eEC_EFFECT_TURI_HANE1, hane_pos, 1, angle, game, EMPTY_NO, 0, 0);
+            break;
+        }
+
+        case 4:
+        case 5:
+        case 6:
+        case 7: 
+        case 8: {
+            s16 type = arg0 - 4;
+            s16 hamon_type[] = { 1, 0, 0, 1, 2 };
+
+            eEC_CLIP->effect_make_proc(eEC_EFFECT_TURI_HAMON, pos, 1, angle, game, EMPTY_NO, hamon_type[type], 0);
+            eEC_CLIP->effect_make_proc(eEC_EFFECT_TURI_HANE0, pos, 1, angle, game, EMPTY_NO, type, 0);
+            break;
+        }
+    }
+}
 
 static void eTM_init(xyz_t pos, int prio, s16 angle, GAME* game, u16 item_name, s16 arg0, s16 arg1) {
     if (eEC_CLIP != NULL) {
@@ -33,16 +82,78 @@ static void eTM_init(xyz_t pos, int prio, s16 angle, GAME* game, u16 item_name, 
 }
 
 static void eTM_ct(eEC_Effect_c* effect, GAME* game, void* ct_arg) {
-    // TODO
+    s16 i;
+    s16 n_drops = 4;
+    s16* angle_p = (s16*)ct_arg;
+    s16 angle = *angle_p;
+
+    effect->effect_specific[5] = angle;
+    switch (effect->arg0) {
+        case 0:
+        case 2:
+            effect->timer = 16;
+            break;
+        case 1:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+            if (effect->arg0 == 4) {
+                n_drops = 3;
+            } else if (effect->arg0 == 7 || effect->arg0 == 8) {
+                n_drops = 2;
+            }
+
+            effect->timer = 20;
+            for (i = 0; i < n_drops; i++) {
+                effect->effect_specific[i] = RANDOM(10);
+            }
+            break;
+    }
 }
 
 static void eTM_mv(eEC_Effect_c* effect, GAME* game) {
-    // TODO
+    s16 i;
+    s16 timer = 16 - effect->timer;
+    s16 n_drops = 4;
+    GAME_PLAY* play = (GAME_PLAY*)game;
+
+    // hack for 30FPS->60FPS
+    if (play->game_frame & 1) {
+        return;
+    }
+
+    switch (effect->arg0) {
+        case 0:
+        case 2:
+            if (effect->timer == 1) {
+                eEC_CLIP->effect_make_proc(eEC_EFFECT_TURI_HAMON, effect->position, 1, effect->effect_specific[5], game, 0, 2, 0);
+            }
+            break;
+        case 1:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+            if (effect->arg0 == 4) {
+                n_drops = 3;
+            } else if (effect->arg0 == 7 || effect->arg0 == 8) {
+                n_drops = 2;
+            }
+
+            for (i = 0; i < n_drops; i++) {
+                if (effect->effect_specific[i] == timer) {
+                    eEC_CLIP->effect_make_proc(eEC_EFFECT_TURI_SUITEKI, effect->position, 1, 0, game, 0, i, n_drops);
+                }
+            }
+            break;
+    }
 }
 
 static void eTM_dw(eEC_Effect_c* effect, GAME* game) {
-    // TODO
-}
-static void eTM_CallEffect(xyz_t pos, int prio, s16 angle, GAME* game, u16 item_name, s16 arg0, s16 arg1) {
-    // TODO
+    // nothing
 }
