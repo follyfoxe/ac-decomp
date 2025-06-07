3f711648da8a6d6d43035f471850d005170ae680
Cuyler36 <Cuyler36@users.noreply.github.com>
2025-06-07 17:26:38 -0400

    Implement & link ac_hanabi_npc1

--- configure.py
+++ configure.py
@@ -1100,7 +1100,7 @@ config.libs = [
             Object(NonMatching, "actor/npc/ac_groundhog_npc0.c"),
             Object(Matching, "actor/npc/ac_halloween_npc.c"),
             Object(Matching, "actor/npc/ac_hanabi_npc0.c"),
-            Object(NonMatching, "actor/npc/ac_hanabi_npc1.c"),
+            Object(Matching, "actor/npc/ac_hanabi_npc1.c"),
             Object(NonMatching, "actor/npc/ac_hanami_npc0.c"),
             Object(NonMatching, "actor/npc/ac_hanami_npc1.c"),
             Object(NonMatching, "actor/npc/ac_harvest_npc0.c"),
--- include/ac_hanabi_npc1.h
+++ include/ac_hanabi_npc1.h
@@ -3,10 +3,25 @@
 
 #include "types.h"
 #include "m_actor.h"
+#include "ac_npc.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef struct hanabi_npc1_actor_s HANABI_NPC1_ACTOR;
+
+typedef void (*aHN1_ACT_PROC)(HANABI_NPC1_ACTOR* actor);
+
+struct hanabi_npc1_actor_s {
+    NPC_ACTOR npc_class;
+    int action;
+    aHN1_ACT_PROC act_proc;
+    s16 dst_pos_x;
+    s16 dst_pos_z;
+    int npc_idx;
+    u8 clap_se_no;
+};
 
 extern ACTOR_PROFILE Hanabi_Npc1_Profile;
 
@@ -15,4 +30,3 @@ extern ACTOR_PROFILE Hanabi_Npc1_Profile;
 #endif
 
 #endif
-
