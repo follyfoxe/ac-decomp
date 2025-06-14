5d1f5e2834eac73533149a9e2adf1c08b4b54924
Norgesnerd <5824576+Norgesnerd@users.noreply.github.com>
2025-06-14 23:59:16 +0200

    Implement & link ef_namida

--- configure.py
+++ configure.py
@@ -1297,7 +1297,7 @@ config.libs = [
             Object(NonMatching, "effect/ef_motiyuge.c"),
             Object(Matching, "effect/ef_muka.c"),
             Object(Matching, "effect/ef_naku.c"),
-            Object(NonMatching, "effect/ef_namida.c"),
+            Object(Matching, "effect/ef_namida.c"),
             Object(Matching, "effect/ef_neboke.c"),
             Object(NonMatching, "effect/ef_neboke_akubi.c"),
             Object(NonMatching, "effect/ef_neboke_awa.c"),
