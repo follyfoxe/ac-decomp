19adcc118925faaa24d41d82c6dd935f3a918bdb
Cuyler36 <Cuyler36@users.noreply.github.com>
2025-06-16 06:54:43 -0400

    Implement & link ef_tumble_dust

--- configure.py
+++ configure.py
@@ -1342,7 +1342,7 @@ config.libs = [
             Object(Matching, "effect/ef_tent_lamp.c"),
             Object(NonMatching, "effect/ef_tumble.c"),
             Object(NonMatching, "effect/ef_tumble_bodyprint.c"),
-            Object(NonMatching, "effect/ef_tumble_dust.c"),
+            Object(Matching, "effect/ef_tumble_dust.c"),
             Object(Matching, "effect/ef_turi_hamon.c"),
             Object(Matching, "effect/ef_turi_hane0.c"),
             Object(Matching, "effect/ef_turi_hane1.c"),
