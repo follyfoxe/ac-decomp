98679be2de4eac634263eca7e94861227300cac8
Cuyler36 <Cuyler36@users.noreply.github.com>
2025-06-16 09:37:54 -0400

    Implement & link ef_string

--- configure.py
+++ configure.py
@@ -1329,7 +1329,7 @@ config.libs = [
             Object(NonMatching, "effect/ef_slip_footprint.c"),
             Object(NonMatching, "effect/ef_soba_yuge.c"),
             Object(NonMatching, "effect/ef_steam.c"),
-            Object(NonMatching, "effect/ef_string.c"),
+            Object(Matching, "effect/ef_string.c"),
             Object(Matching, "effect/ef_suisou_awa.c"),
             Object(Matching, "effect/ef_swing_axe.c"),
             Object(Matching, "effect/ef_swing_net.c"),
