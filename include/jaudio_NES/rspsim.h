bdb925908ac91d33b30d33d2f1adefd349beda8e
Cuyler36 <Cuyler36@users.noreply.github.com>
2025-06-28 09:53:43 -0400

    jaudio_NES/rspsim: 95%

--- include/jaudio_NES/rspsim.h
+++ include/jaudio_NES/rspsim.h
@@ -3,6 +3,10 @@
 
 #include "types.h"
 
-extern void RspStart2(u32* pTaskCmds, s32 allTasks, s32 param_3);
+#define RSPSIM_MODE_INIT 0
+#define RSPSIM_MODE_MOVE 1
+
+extern s32 RspStart(u32* pTaskCmds, s32 allTasks);
+extern void RspStart2(u32* pTaskCmds, s32 allTasks, s32 mode);
 
 #endif
