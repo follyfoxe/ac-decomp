2addbb1141af12458d7c8e2dbb3e264f3a825ebb
roeming <brother64youyou@gmail.com>
2025-07-26 15:34:06 -0400

    speedup pal16 and vtx extraction

--- tools/converters/pal16dis.py
+++ tools/converters/pal16dis.py
@@ -5,15 +5,11 @@ import struct
 def convert_binary_to_vtx_c_source(src_path, dest_path):
     # Load data
     with open(src_path, "rb") as binary_file, open(dest_path, "w") as c_file:
-        while True:
-            chunk = binary_file.read(2)  # 2 bytes for each 16-bit palette entry
-
-            if len(chunk) < 2:
-                break
-
-            pal = struct.unpack(">H", chunk[0:2])  # 2 bytes
-
-            c_file.write(f"{hex(pal[0])}, ")
+        all_bin = binary_file.read()
+        # parse all data as an array of u16,
+        # use tuple's str method to create "(1, 2, 3)", and then sub-string to remove the parentheses
+        c_file.write(
+            str(struct.unpack(">" + "H" * (len(all_bin) // 2), all_bin))[1:-1])
 
 
 def main():
@@ -21,7 +17,8 @@ def main():
         description="Converts a binary file to an array of u16 type"
     )
     parser.add_argument("src_path", type=str, help="Binary source file path")
-    parser.add_argument("dest_path", type=str, help="Destination C include file path")
+    parser.add_argument("dest_path", type=str,
+                        help="Destination C include file path")
 
     args = parser.parse_args()
     convert_binary_to_vtx_c_source(args.src_path, args.dest_path)
